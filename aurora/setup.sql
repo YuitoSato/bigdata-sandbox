ec788a8deb073e1c7a417ab209ea26990e3157ea
Yuito <yuitosato.w@gmail.com>
2022-05-22 22:42:23 +0900

    edit

--- aurora/setup.sql
+++ aurora/setup.sql
@@ -1,22 +1,25 @@
-CREATE TABLE tenants
+
+CREATE TABLE TENANTS
 (
-    tenant_id integer               NOT NULL
-        constraint tenants_pk primary key,
-    name      character varying(30) NOT NULL
+    TENANT_ID INTEGER               NOT NULL
+        CONSTRAINT TENANTS_PK PRIMARY KEY,
+    NAME      CHARACTER varying(30) NOT NULL
 );
 
-CREATE TABLE accounts
+CREATE TABLE ACCOUNTS
 (
-    account_id integer               NOT NULL
-        constraint accounts_pk primary key,
-    tenant_id  integer               NOT NULL
-        constraint fk_accounts_tenants
-            references tenants
-            on update restrict on delete restrict,
-    name       character varying(30) NOT NULL
+    account_id INTEGER               NOT NULL
+        CONSTRAINT ACCOUNTS_PK PRIMARY KEY,
+    TENANT_ID  INTEGER               NOT NULL
+        CONSTRAINT FK_ACCOUNTS_TENANTS
+            REFERENCES TENANTS
+            ON UPDATE RESTRICT ON DELETE RESTRICT,
+    NAME       CHARACTER VARYING(30) NOT NULL
 );
 
 SELECT pg_create_logical_replication_slot('airbyte_slot', 'pgoutput');
-ALTER TABLE tenants REPLICA IDENTITY DEFAULT;
-ALTER TABLE accounts REPLICA IDENTITY DEFAULT;
-CREATE PUBLICATION airbyte_publication FOR TABLE tenants, accounts;
+ALTER TABLE TENANTS
+    REPLICA IDENTITY DEFAULT;
+ALTER TABLE ACCOUNTS
+    REPLICA IDENTITY DEFAULT;
+CREATE PUBLICATION AIRBYTE_PUBLICATION FOR TABLE TENANTS, ACCOUNTS;
--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,7 @@ resource "aws_rds_cluster" "bigdata-sandbox-aurora-cluster" {
     "bigdata-sandbox-aurora-cluster-instance-1",
   ]
   copy_tags_to_snapshot               = true
-  database_name                       = "bigdata_sandbox_aurora_db"
+  database_name                       = "postgres"
   db_cluster_parameter_group_name     = "bigdata-sandbox-aurora-cluster-parameter-group"
   db_subnet_group_name                = "default-vpc-0a2a725f51e785674"
   deletion_protection                 = false
@@ -128,7 +128,7 @@ resource "aws_dms_endpoint" "bigdata-sandbox-aurora-cluster-source-endpoint" {
   engine_name   = "aurora-postgresql"
   kms_key_arn   = "arn:aws:kms:ap-northeast-1:060507316679:key/21d148da-046c-45ee-a60b-0a3f3b8888ad"
   port          = 5432
-  server_name   = aws_rds_cluster.bigdata-sandbox-aurora-cluster.endpoint
+  server_name   = tolist(aws_route53_record.bigdata-sandbox-internal-aurora-route53-record.records)[0]
   ssl_mode      = "none"
   tags          = {}
   tags_all      = {}
@@ -157,7 +157,243 @@ resource "aws_dms_endpoint" "bigdata-sandbox-aurora2snowflake-s3-target-endpoint
   }
 }
 
-
-#
-#resource "aws_dms_replication_task" "bigdata-sandbox-aurora2snowflake-dms-task" {
-#}
+resource "aws_dms_replication_task" "bigdata-sandbox-aurora2snowflake-dms-task" {
+  migration_type            = "full-load-and-cdc"
+  replication_instance_arn  = aws_dms_replication_instance.bigdata-sandbox-aurora2instance-rpl-instance.replication_instance_arn
+  replication_task_id       = "bigdata-sandbox-aurora2snowflake-dms-task"
+  replication_task_settings = jsonencode(
+    {
+      BeforeImageSettings                 = null
+      ChangeProcessingDdlHandlingPolicy   = {
+        HandleSourceTableAltered   = true
+        HandleSourceTableDropped   = true
+        HandleSourceTableTruncated = true
+      }
+      ChangeProcessingTuning              = {
+        BatchApplyMemoryLimit         = 500
+        BatchApplyPreserveTransaction = true
+        BatchApplyTimeoutMax          = 30
+        BatchApplyTimeoutMin          = 1
+        BatchSplitSize                = 0
+        CommitTimeout                 = 1
+        MemoryKeepTime                = 60
+        MemoryLimitTotal              = 1024
+        MinTransactionSize            = 1000
+        StatementCacheSize            = 50
+      }
+      CharacterSetSettings                = null
+      ControlTablesSettings               = {
+        ControlSchema                 = ""
+        FullLoadExceptionTableEnabled = false
+        HistoryTableEnabled           = false
+        HistoryTimeslotInMinutes      = 5
+        StatusTableEnabled            = false
+        SuspendedTablesTableEnabled   = false
+      }
+      ErrorBehavior                       = {
+        ApplyErrorDeletePolicy                      = "IGNORE_RECORD"
+        ApplyErrorEscalationCount                   = 0
+        ApplyErrorEscalationPolicy                  = "LOG_ERROR"
+        ApplyErrorFailOnTruncationDdl               = false
+        ApplyErrorInsertPolicy                      = "LOG_ERROR"
+        ApplyErrorUpdatePolicy                      = "LOG_ERROR"
+        DataErrorEscalationCount                    = 0
+        DataErrorEscalationPolicy                   = "SUSPEND_TABLE"
+        DataErrorPolicy                             = "LOG_ERROR"
+        DataTruncationErrorPolicy                   = "LOG_ERROR"
+        EventErrorPolicy                            = "IGNORE"
+        FailOnNoTablesCaptured                      = true
+        FailOnTransactionConsistencyBreached        = false
+        FullLoadIgnoreConflicts                     = true
+        RecoverableErrorCount                       = -1
+        RecoverableErrorInterval                    = 5
+        RecoverableErrorStopRetryAfterThrottlingMax = true
+        RecoverableErrorThrottling                  = true
+        RecoverableErrorThrottlingMax               = 1800
+        TableErrorEscalationCount                   = 0
+        TableErrorEscalationPolicy                  = "STOP_TASK"
+        TableErrorPolicy                            = "SUSPEND_TABLE"
+      }
+      FailTaskWhenCleanTaskResourceFailed = false
+      FullLoadSettings                    = {
+        CommitRate                      = 10000
+        CreatePkAfterFullLoad           = false
+        MaxFullLoadSubTasks             = 8
+        StopTaskCachedChangesApplied    = false
+        StopTaskCachedChangesNotApplied = false
+        TargetTablePrepMode             = "DROP_AND_CREATE"
+        TransactionConsistencyTimeout   = 600
+      }
+      Logging                             = {
+        EnableLogging = true
+        LogComponents = [
+          {
+            Id       = "TRANSFORMATION"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "SOURCE_UNLOAD"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "IO"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "TARGET_LOAD"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "PERFORMANCE"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "SOURCE_CAPTURE"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "SORTER"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "REST_SERVER"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "VALIDATOR_EXT"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "TARGET_APPLY"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "TASK_MANAGER"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "TABLES_MANAGER"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "METADATA_MANAGER"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "FILE_FACTORY"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "COMMON"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "ADDONS"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "DATA_STRUCTURE"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "COMMUNICATION"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            Id       = "FILE_TRANSFER"
+            Severity = "LOGGER_SEVERITY_DEFAULT"
+          },
+        ]
+      }
+      LoopbackPreventionSettings          = null
+      PostProcessingRules                 = null
+      StreamBufferSettings                = {
+        CtrlStreamBufferSizeInMB = 5
+        StreamBufferCount        = 3
+        StreamBufferSizeInMB     = 8
+      }
+      TTSettings                          = {
+        EnableTT         = false
+        TTRecordSettings = null
+        TTS3Settings     = null
+      }
+      TargetMetadata                      = {
+        BatchApplyEnabled            = false
+        FullLobMode                  = false
+        InlineLobMaxSize             = 0
+        LimitedSizeLobMode           = false
+        LoadMaxFileSize              = 0
+        LobChunkSize                 = 0
+        LobMaxSize                   = 0
+        ParallelApplyBufferSize      = 0
+        ParallelApplyQueuesPerThread = 0
+        ParallelApplyThreads         = 0
+        ParallelLoadBufferSize       = 0
+        ParallelLoadQueuesPerThread  = 0
+        ParallelLoadThreads          = 0
+        SupportLobs                  = false
+        TargetSchema                 = ""
+        TaskRecoveryTableEnabled     = false
+      }
+    }
+  )
+  source_endpoint_arn       = aws_dms_endpoint.bigdata-sandbox-aurora-cluster-source-endpoint.endpoint_arn
+  table_mappings            = jsonencode(
+    {
+      rules = [
+        {
+          object-locator = {
+            column-name = "%"
+            schema-name = "%"
+            table-name  = "%"
+          }
+          old-value      = null
+          rule-action    = "convert-lowercase"
+          rule-id        = "223946717"
+          rule-name      = "223946717"
+          rule-target    = "column"
+          rule-type      = "transformation"
+          value          = null
+        },
+        {
+          object-locator = {
+            schema-name = "%"
+            table-name  = "%"
+          }
+          old-value      = null
+          rule-action    = "convert-lowercase"
+          rule-id        = "223935588"
+          rule-name      = "223935588"
+          rule-target    = "table"
+          rule-type      = "transformation"
+          value          = null
+        },
+        {
+          object-locator = {
+            schema-name = "%"
+          }
+          old-value      = null
+          rule-action    = "convert-lowercase"
+          rule-id        = "223924348"
+          rule-name      = "223924348"
+          rule-target    = "schema"
+          rule-type      = "transformation"
+          value          = null
+        },
+        {
+          object-locator = {
+            schema-name = "%"
+            table-name  = "%"
+          }
+          rule-action    = "include"
+          rule-id        = "220834611"
+          rule-name      = "220834611"
+          rule-type      = "selection"
+        },
+      ]
+    }
+  )
+  tags                      = {}
+  tags_all                  = {}
+  target_endpoint_arn       = aws_dms_endpoint.bigdata-sandbox-aurora2snowflake-s3-target-endpoint.endpoint_arn
+}
